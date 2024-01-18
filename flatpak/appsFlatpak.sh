62edb3b6681c03a4486823af4a5d628ed7ac0957
F343 <129131252+DL343@users.noreply.github.com>
2024-01-18 01:04:10 -0600

    add apps

--- flatpak/appsFlatpak.sh
+++ flatpak/appsFlatpak.sh
@@ -4,6 +4,16 @@
 flatpak install flathub net.cozic.joplin_desktop
 
 ## Waterfox 
-#flatpak install flathub net.waterfox.waterfox
+flatpak install flathub net.waterfox.waterfox
 
+## VLC
+flatpak install flathub org.videolan.VLC
 
+## Geany
+flatpak install flathub org.geany.Geany
+
+## gThumb
+flatpak install flathub org.gnome.gThumb
+
+## Bleachbit
+flatpak install flathub org.bleachbit.BleachBit
