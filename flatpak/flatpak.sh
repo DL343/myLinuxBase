987b21cfbe52098bfed9e4dd0e13e889a5d7f2d1
DL343 <129131252+DL343@users.noreply.github.com>
2024-01-24 22:11:55 -0600

    minor changes and add 'grub'

--- flatpak/flatpak.sh
+++ flatpak/flatpak.sh
@@ -21,13 +21,7 @@ flatpak install flathub -y net.waterfox.waterfox
 ## VLC
 flatpak install flathub -y org.videolan.VLC
 
-## Geany
-flatpak install flathub -y org.geany.Geany
-
 ## gThumb
 flatpak install flathub -y org.gnome.gThumb
-
-## Bleachbit
-flatpak install flathub -y org.bleachbit.BleachBit
 
 
--- system/system.sh
+++ system/system.sh
@@ -17,11 +17,12 @@ sudo nala install p7zip-full arandr gvfs network-manager-gnome -y
 sudo nala install brightnessctl acpi lightdm lightdm-gtk-greeter -y
 
 sudo nala install rofi redshift gnome-screenshot -y
-sudo nala install thunar lxappearance sakura -y
+sudo nala install thunar lxappearance sakura  -y
 
 
 ## OPCIONALES
-## sudo nala install gthumb bleachbit geany xarchiver vlc firefox-esr -y
+sudo nala install geany bleachbit -y
+## sudo nala install gthumb  xarchiver vlc firefox-esr -y
 
 
 
@@ -33,8 +34,12 @@ sudo sed -i '36a Option "Tapping" "true"' /usr/share/X11/xorg.conf.d/40-libinput
 #sudo nala install policykit-1-gnome
 #echo "/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &" >> $HOME/.icewm/startup  
 
-echo '########## GENERACION DE CARPETAS BASICAS ##########'
+echo '########## GENERACION DE CARPETAS DE USUARIO BASICAS ##########'
 xdg-user-dirs-update 
+
+#echo '########## CONFIGURACION DE GRUB ##########'
+sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g' /etc/default/grub
+sudo update-grub
 
 
 
