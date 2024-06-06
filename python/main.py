f3feb31cb413d43baa5676f8db3d496e31f514f7
DL343 <129131252+DL343@users.noreply.github.com>
2024-06-06 16:03:14 -0600

    pruebas con python

---
+++ python/main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root=Tk()
+
+root.title("myLinuxBase")
+
+root.resizable(1,1)
+
+root.iconbitmap("icono.ico")
+
+root.mainloop()
+
+
+
+#import subprocess
+
+#subprocess.call(['sudo', 'apt', 'install', 'geany'])
+
+#subprocess.call(['sudo', 'apt', 'install', 'nano'])
