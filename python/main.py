c50ce625b1f339c48558442e9750cce12c8d884d
DL343 <129131252+DL343@users.noreply.github.com>
2024-06-07 16:00:22 -0600

    pequeños cambios

--- python/main.py
+++ python/main.py
@@ -6,9 +6,28 @@ root.title("myLinuxBase")
 
 root.resizable(1,1)
 
-root.iconbitmap("icono.ico")
+##root.iconbitmap("icono.ico")
 
-root.mainloop()
+#root.geometry("700x400")
+
+root.config(bg="#555555")
+
+## Creacion del frame
+miFrame = Frame()
+
+## Enpaquetarlo/Instrducir a root
+#miFrame.pack(fill="both", expand="1")
+miFrame.pack()
+
+## Configuracion
+miFrame.config(bg="red")
+miFrame.config(width="500",height="500")
+
+miFrame.config(bd="35")
+miFrame.config(relief="groove")
+
+miFrame.config(cursor="hand2")
+#miFrame.config(cursor="pirate")
 
 
 
@@ -17,3 +36,7 @@ root.mainloop()
 #subprocess.call(['sudo', 'apt', 'install', 'geany'])
 
 #subprocess.call(['sudo', 'apt', 'install', 'nano'])
+
+
+
+root.mainloop()
