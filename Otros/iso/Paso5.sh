92df7258b7f33a0e714951f3e0aefe2ee8e4f218
DL343 <129131252+DL343@users.noreply.github.com>
2024-08-30 13:14:39 -0600

    update

--- Otros/iso/Paso5.sh
+++ Otros/iso/Paso5.sh
@@ -36,7 +36,7 @@ chown live /home/live -R
 ################### A ELIMINAR #####################
 ####################################################
 
-#sudo apt remove aspell-es chafa cups-pk-helper debian-reference-es debian-reference-common fonts-liberation git ispanish task-spanish manpages-es system-config-printer system-config-printer-common python3-gi-cairo python3-cairo system-config-printer-udev python3-cupshelpers python3-cups python3-smbc rtkit 
+sudo apt remove aspell-es chafa cups-pk-helper debian-reference-es debian-reference-common fonts-liberation git ispanish task-spanish manpages-es system-config-printer system-config-printer-common python3-gi-cairo python3-cairo system-config-printer-udev python3-cupshelpers python3-cups python3-smbc rtkit
 ##blueman
 
 ####################################################
