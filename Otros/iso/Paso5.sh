4d94ce5e970d39859e34eef138179b1bf04fc76f
DL343 <129131252+DL343@users.noreply.github.com>
2024-08-30 13:08:45 -0600

    update

--- Otros/iso/Paso5.sh
+++ Otros/iso/Paso5.sh
@@ -48,9 +48,9 @@ echo "$nombreDistro" > /etc/hostname
 ####################################################
 ################# DISPLAY MANAGER ##################
 ####################################################
-sed -i '/autologin-user=/c autologin-user=live/' /etc/lightdm/lightdm.conf
-sed -i '/autologin-user-timeout=0/c autologin-user-timeout=0/' /etc/lightdm/lightdm.conf
-sed -i '/autologin-session=/c autologin-session=icewm-session/' /etc/lightdm/lightdm.conf 
+sed -i '/autologin-user=/c autologin-user=live' /etc/lightdm/lightdm.conf
+sed -i '/autologin-user-timeout=0/c autologin-user-timeout=0' /etc/lightdm/lightdm.conf
+sed -i '/autologin-session=/c autologin-session=icewm-session' /etc/lightdm/lightdm.conf 
 
 
 ####################################################
