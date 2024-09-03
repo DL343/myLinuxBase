062a60c10f31d86a8865451edd795e2d56810c07
DL343 <129131252+DL343@users.noreply.github.com>
2024-09-03 17:57:56 -0600

    update

--- añadidos/withoutDM/changeDM.sh
+++ añadidos/withoutDM/changeDM.sh
@@ -13,12 +13,41 @@ read -p "¿Que WM desea aplicar?: " WM
 
 ## Configuracion de startx
 echo "$WM" > ~/.xinitrc
+echo 'Listo
+
+
+'
+
+
+
+echo "Aplicando ajustes..." 
 
  
+if grep -q '############## CUSTOM ##################' $HOME/.profile
+then
+	echo 'Existe este ajuste, omitiendo este paso...' 
+else
+
+	echo '
+    ########################################
+	############## CUSTOM ##################
+    ######################################## 
+	
+	## Inicio automatico del WM despues del inicio de sesion 
+	if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
+	  exec startx
+	fi
+
+	## Ajuste adicional
+	dbus-update-activation-environment DISPLAY
+
+
+	'  >> $HOME/.profile
+
+fi
 
 echo "
 --------------
-     LISTO
+    LISTO
 --------------
-
 "
