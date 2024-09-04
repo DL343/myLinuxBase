810a4df121d4d010c861cdde35c8737243240b19
DL343 <129131252+DL343@users.noreply.github.com>
2024-09-03 18:53:51 -0600

    update

--- WM/iceWM/iceWM.sh
+++ WM/iceWM/iceWM.sh
@@ -20,5 +20,8 @@ echo "
 echo "## Agregar configuraciones" 
 cp -rp ./getICEWMConfig/.icewm/ $HOME/
 
+## Mostar fecha y hora IceWM
+sed -i '/TimeFormat=/c  TimeFormat="%x | %H:%M"' $HOME/.icewm/preferences
+
 
 echo "## Listo"
