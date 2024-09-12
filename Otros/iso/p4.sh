c4ffebc93c016ba2a6fe3d1223d5077a0e18ef8e
DL343 <129131252+DL343@users.noreply.github.com>
2024-09-12 10:25:18 -0600

    update

--- Otros/iso/p4.sh
+++ Otros/iso/p4.sh
@@ -357,25 +357,23 @@ then
 	## ***Todo esta comentado, al instalar paquetes piden regresar el archivo como estaba
 	## Evita que ciertos módulos del kernel del microcodigo del procesador se carguen automáticamente
 	## AMD 
-	echo '
-	# The microcode module attempts to apply a microcode update when
-	# it autoloads.  This is not always safe, so we block it by default.
+	echo '# The microcode module attempts to apply a microcode update when
+# it autoloads.  This is not always safe, so we block it by default.
 
-	## Debian default
-	blacklist microcode
+## Debian default
+blacklist microcode
 
-	' > /etc/modprobe.d/amd64-microcode-blacklist.conf
+' > /etc/modprobe.d/amd64-microcode-blacklist.conf
 
 
 	## INTEL
-	echo '
-	# The microcode module attempts to apply a microcode update when
-	# it autoloads.  This is not always safe, so we block it by default.
+	echo '# The microcode module attempts to apply a microcode update when
+# it autoloads.  This is not always safe, so we block it by default.
 
-	## Debian default
-	blacklist microcode
+## Debian default
+blacklist microcode
 
-	' > /etc/modprobe.d/intel-microcode-blacklist.conf
+' > /etc/modprobe.d/intel-microcode-blacklist.conf
 		
 	update-initramfs -u
 		
--- Otros/iso/testing.sh
+++ Otros/iso/testing.sh
@@ -23,5 +23,34 @@ echo "#########################################"
 
 
 
+	echo "
+	########################################################################
+	####################### MODULOS DEL KERNEL ############################# 
+	########################################################################
+	"
+	## ***Todo esta comentado, al instalar paquetes piden regresar el archivo como estaba
+	## Evita que ciertos módulos del kernel del microcodigo del procesador se carguen automáticamente
+	## AMD 
+	echo '# The microcode module attempts to apply a microcode update when
+# it autoloads.  This is not always safe, so we block it by default.
 
+## Debian default
+blacklist microcode
+
+' > /etc/modprobe.d/amd64-microcode-blacklist.conf
+
+
+	## INTEL
+	echo '# The microcode module attempts to apply a microcode update when
+# it autoloads.  This is not always safe, so we block it by default.
+
+## Debian default
+blacklist microcode
+
+' > /etc/modprobe.d/intel-microcode-blacklist.conf
+		
+	update-initramfs -u
+		
+		
+	
 
