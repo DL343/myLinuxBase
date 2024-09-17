99dae8568957381a4ae788caab8bad0c98a3f67e
DL343 <129131252+DL343@users.noreply.github.com>
2024-09-16 18:09:32 -0600

    update

--- Otros/iso/myApps.sh
+++ Otros/iso/myApps.sh
@@ -189,7 +189,8 @@ fi
 
 
 sudo apt remove avahi-daemon   git  at-spi2-core  rtkit
-
 sudo apt remove xdg-desktop-portal   xdg-desktop-portal-gtk
+
+
 #sudo apt remove cron
 #sudo apt remove aspell-es chafa cups-pk-helper debian-reference-es debian-reference-common fonts-liberation ispanish task-spanish manpages-es system-config-printer system-config-printer-common python3-gi-cairo python3-cairo system-config-printer-udev python3-cupshelpers python3-cups python3-smbc 
