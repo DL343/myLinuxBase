192744a0bcc456f3f1dd03dd075baf258a34c3c0
DL343 <129131252+DL343@users.noreply.github.com>
2024-10-06 08:01:31 -0600

    update

--- Otros/iso/kde.sh
+++ Otros/iso/kde.sh
@@ -185,6 +185,108 @@ fi
 
 sed -i '/mksq_opt="-comp xz -Xbcj x86"/c mksq_opt="-comp xz -Xbcj x86"' /etc/refractasnapshot.conf
 
+
+
+
+cat << 'EOF' > /bin/loc-oswelcome.sh
+#!/usr/bin/env bash
+#=======================HEADER========================================|
+#AUTOR
+# Nicolas Longardi 'Locos por Linux' <nico@locosporlinux.com>
+# Jefferson 'Slackjeff' Rocha <root@slackjeff.com.br>
+# Creado totalmente en bash + YAD.
+#===================================| VARIABLES
+# Nombre del programa
+PRG='Loc-OS Welcome'
+
+# Version
+VERSION='1.0'
+
+# Archivo temporário para exclusion
+temp="/etc/xdg/autostart/loc-oswelcome.desktop"
+
+# Ubicacion de los iconos
+icon='/usr/share/pixmaps/loc-oswelcome'
+
+#===================================| INICIO
+# Menu Principal
+ yad --title="$PRG"            \
+    --width="800"              \
+    --height="450"             \
+    --text-align=left          \
+    --center                   \
+    --image-on-top             \
+    --undecorated              \
+    --fontname="monospace 56"  \
+    --image="${icon}/logo.png" \
+    --borders=10               \
+    --text="
+<big><b>Loc-OS</b> Welcome</big>
+
+Welcome, below you can check some options to install or configure the system.
+Remember that the password of the <big><b>live</b></big> user is <big><b>live</b></big> and that of <big><b>root</b></big> is <big><b>root</b></big>."   \
+   --form  --columns 4                                                                                     \
+    --field="<b>Install</b>!${icon}/install.png!Install Loc-OS Linux on the HDD/SSD":BTN "/usr/bin/install-loc-os " \
+    --field="<b>Website</b>!${icon}/site.png!Official site of Loc-OS":BTN "xdg-open https://loc-os.sourceforge.io/"           \
+    --field="<b>LPKG</b>!${icon}/lpkg.png!Loc-OS Package Manager":BTN "/opt/Loc-OS-LPKG/.sudo/sudogui /opt/Loc-OS-LPKG/LpkgGui"   \ "
+yad --width='500'
+     --height='100'
+     --center
+     --undecorated
+     --image="${icon}/sobreyad.png"
+     --borders=5
+     --text-align=left
+     --text='
+
+<big><b>$PRG</b></big>
+
+Loc-OS Linux is a free GNU/Linux distribution, oriented to low resource consumption. Free from systemd and based on Debian stable.
+
+<b>Version:</b> $VERSION
+<b>License:</b> GPLv3
+<b>Contact:</b> nico@locosporlinux.com'
+     --button="Exit":1
+"                                                                                                           \ "
+"                                                                                                       \
+    --field="<b>About</b>!${icon}/sobre.png!About this proyect":BTN  "xdg-open ${sites[4]}"            \
+    --button="Exit":1
+
+# El inicio automatico debe ser apenas en el primer boot.
+# Luego debe ser excluido.
+if [[ -e "$temp" ]]; then
+    rm "$temp"
+fi
+
+EOF
+
+
+echo "
+[Desktop Entry]
+Type=Application
+Version=1.0
+Name=Install Loc-OS
+GenericName=System Installer
+Keywords=calamares;system;installer;
+TryExec=calamares
+Exec=sh -c "pkexec calamares"
+Comment=Calamares — System Installer
+Icon=/usr/share/icons/hicolor/48x48/apps/calamares-loc-os.png
+Terminal=false
+StartupNotify=true
+Categories=Qt;System;
+X-AppStream-Ignore=true
+" > /home/live/Desktop/Instalar.desktop
+
+
+
+
+
+
+
+
+
+
+
 ##################################################
 ## Elimina paquetes adicionales
 ##################################################
@@ -198,3 +300,4 @@ apt -y purge network-manager
 apt -y autoremove
 
 apt -y reinstall welcome-loc-os
+
