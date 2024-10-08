95b79e1bb3439dd47b22f95a650d9ec70746d9f7
DL343 <129131252+DL343@users.noreply.github.com>
2024-10-08 02:24:03 -0600

    update

--- añadidos/Toggles/setToogles.sh
+++ añadidos/Toggles/setToogles.sh
@@ -132,6 +132,50 @@ chmod +x $HOME/.config/scripts/toggle_zenityCalendar.sh
 
 
 
+echo "
+#############################################
+  AJUSTE SEGUNDO DISPOSITIVO VOLUMEN (CALENDARIO)
+#############################################
+"
 
+cat << 'EOF' > $HOME/.config/scripts/twoVolume.sh
+#!/bin/bash
 
+# Obtén la lista de dispositivos de salida
+salidas=$(pactl list short sinks | awk '{print $2}')
+
+# Convierte la lista en un array
+IFS=$'\n' read -rd '' -a dispositivos <<< "$salidas"
+
+# Verifica si hay al menos dos dispositivos
+if [ ${#dispositivos[@]} -lt 2 ]; then
+    echo "No hay suficientes dispositivos de salida."
+    exit 1
+fi
+
+# Establece el segundo dispositivo
+segundo_dispositivo=${dispositivos[1]}
+
+# Acciones según el primer argumento
+case "$1" in
+    +)
+        # Verifica que se especifique una cantidad
+        cantidad=${2:-5}  # Si no se especifica, sube 5%
+        pactl set-sink-volume "$segundo_dispositivo" +${cantidad}%
+        ;;
+    -)
+        # Verifica que se especifique una cantidad
+        cantidad=${2:-5}  # Si no se especifica, baja 5%
+        pactl set-sink-volume "$segundo_dispositivo" -${cantidad}%
+        ;;
+    mute)
+        pactl set-sink-mute "$segundo_dispositivo" toggle
+        ;;
+    *)
+        echo "Opcion incorrecta"
+        ;;
+esac
+EOF
+
+chmod +x $HOME/.config/scripts/twoVolume.sh
 
