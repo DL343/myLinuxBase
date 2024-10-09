58ccfd2a10a3f6700f002ad31153d005f4bca6df
DL343 <129131252+DL343@users.noreply.github.com>
2024-10-09 05:13:32 -0600

    update

--- Otros/iso/p4_Dependencias.sh
+++ Otros/iso/p4_Dependencias.sh
@@ -68,61 +68,6 @@ echo "
 ########################################################################
 "
 
-echo "
-#!/bin/sh
-#
-# Writes the final sources.list file
-#
+cp ./apt/sources-final /sbin/
 
-CHROOT=$(mount | grep proc | grep calamares | awk '{print $3}' | sed -e "s#/proc##g")
-RELEASE=\"bookworm\"
-
-cat << EOF > $CHROOT/etc/apt/sources.list
-# See https://wiki.debian.org/SourcesList for more information.
-deb http://deb.debian.org/debian $RELEASE main contrib non-free-firmware
-## deb-src http://deb.debian.org/debian $RELEASE main contrib non-free-firmware
-
-deb http://deb.debian.org/debian $RELEASE-updates main
-## deb-src http://deb.debian.org/debian $RELEASE-updates main
-
-deb http://security.debian.org/debian-security/ $RELEASE-security main
-## deb-src http://security.debian.org/debian-security/ $RELEASE-security main
-EOF
-
-exit 0
-
-" > /sbin/sources-final
-
-
-
-cat << 'EOF' > /sbin/sources-media
-#!/bin/sh
-
-CHROOT=$(mount | grep proc | grep calamares | awk '{print $3}' | sed -e "s#/proc##g")
-MEDIUM_PATH="/run/live/medium"
-RELEASE="bookworm"
-
-if [ "$1" = "-u" ]; then
-    umount $CHROOT/$MEDIUM_PATH
-    rm $CHROOT/etc/apt/sources.list.d/debian-live-media.list
-    chroot $CHROOT apt-get update
-    exit 0
-fi
-
-# Remove the base sources, we will configure sources in a later phase
-rm -f $CHROOT/etc/apt/sources.list.d/base.list
-
-mkdir -p $CHROOT/$MEDIUM_PATH
-mount --bind $MEDIUM_PATH $CHROOT/$MEDIUM_PATH
-echo "deb [trusted=yes] file:$MEDIUM_PATH $RELEASE main" > $CHROOT/etc/apt/sources.list.d/debian-live-media.list
-echo "QT_QPA_PLATFORMTHEME=gtk2" > $CHROOT/etc/environment
-chroot $CHROOT apt-get update
-# Attempt safest way to remove cruft
-rmdir $CHROOT/run/live/medium
-rmdir $CHROOT/run/live
-
-exit 0
-
-EOF
-
-
+cp ./apt/sources-media /sbin/
