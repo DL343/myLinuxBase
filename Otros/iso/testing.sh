442cd5a321d5e01ab5a7fe6813fe40b0bf97ce26
DL343 <129131252+DL343@users.noreply.github.com>
2024-10-10 01:00:54 -0600

    update

--- Otros/iso/testing.sh
+++ Otros/iso/testing.sh
@@ -22,15 +22,3 @@ fi
 echo "#########################################"
 
 
-
-
-######## ICEWM: DESHABILITAR POLKIT GNOME EN LIVE
-if grep -q "#/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &" $HOME/.icewm/startup
-then
-	echo "Existe el ajuste, omitiendo este paso..."
-else
-
-	sed -i '/\/usr\/lib\/policykit-1-gnome\/polkit-gnome-authentication-agent-1 &/c #\/usr\/lib\/policykit-1-gnome\/polkit-gnome-authentication-agent-1 &' $HOME/.icewm/startup
-
-fi
-
