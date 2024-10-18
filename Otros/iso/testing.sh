94e6b914b98ba4ebbb1cecae6b3f371ccb036256
DL343 <129131252+DL343@users.noreply.github.com>
2024-10-18 07:37:47 -0600

    update

--- Otros/iso/testing.sh
+++ Otros/iso/testing.sh
@@ -84,6 +84,44 @@ sudo apt install breeze-icon-theme plasma-discover qml-module-org-kde-purpose li
 apt -y purge kdeconnect
 
 
+##### SYSVINIT: ACTUALIZACION DE REPOSITORIOS AL ARRANQUE
+echo '### BEGIN INIT INFO
+# Provides:          apt-update
+# Required-Start:    $local_fs $remote_fs lightdm
+# Required-Stop:     $local_fs $remote_fs lightdm
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: apt update on startup
+# Description:       apt update on startup
+#                    
+#                    
+### END INIT INFO
+
+
+case "$1" in
+    start)
+        echo "Starting Repository updater: apt update"
+        (apt-get update > /var/log/apt-update.log 2>&1) &
+        ;;
+    stop)
+        #
+        ;;
+    *)
+        echo "Usage: /etc/init.d/apt-update {start|stop}"
+        exit 1
+        ;;
+esac
+
+exit 0
+
+' > /etc/init.d/apt-update
+
+chmod +x /etc/init.d/apt-update
+
+sudo update-rc.d apt-update defaults 
+
+
+
 
 
 
