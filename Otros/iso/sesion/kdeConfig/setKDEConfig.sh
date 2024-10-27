2076b4d40d1679b5eb8c2a3a193eaf837106dab6
DL343 <129131252+DL343@users.noreply.github.com>
2024-10-27 06:21:08 -0600

    update

--- Otros/iso/sesion/kdeConfig/setKDEConfig.sh
+++ Otros/iso/sesion/kdeConfig/setKDEConfig.sh
@@ -4,7 +4,7 @@ cp ./.gtkrc-2.0   /home/live/
 cp ./.gtkrc-2.0   /etc/skel/
 
 cd ./.config.fake/
-cp -r gtk-3.0/ gtk-4.0/ kdedefaults/ gtkrc gtkrc-2.0 kactivitymanagerd-pluginsrc kactivitymanagerdrc kded5rc kdeglobals krunnerrc ksmserverrc kwalletrc kwinrc plasma-org.kde.plasma.desktop-appletsrc  plasmashellrc /home/live/.config
-cp -r gtk-3.0/ gtk-4.0/ kdedefaults/ gtkrc gtkrc-2.0 kactivitymanagerd-pluginsrc kactivitymanagerdrc kded5rc kdeglobals krunnerrc ksmserverrc kwalletrc kwinrc plasma-org.kde.plasma.desktop-appletsrc  plasmashellrc /etc/skel/.config
+cp -r gtk-3.0/ gtk-4.0/ kdedefaults/  akregatorrc  kactivitymanagerdrc  kcminputrc  kconf_updaterc  kded5rc  kdeglobals  kmixrc  krunnerrc  kscreenlockerrc  ksmserverrc    kwalletrc  kwinrc  kwinrulesrc  kxkbrc  plasma-localerc  plasma-org.kde.plasma.desktop-appletsrc  plasmashellrc  powerdevilrc  powermanagementprofilesrc  /home/live/.config
+cp -r gtk-3.0/ gtk-4.0/ kdedefaults/  akregatorrc  kactivitymanagerdrc  kcminputrc  kconf_updaterc  kded5rc  kdeglobals  kmixrc  krunnerrc  kscreenlockerrc  ksmserverrc    kwalletrc  kwinrc  kwinrulesrc  kxkbrc  plasma-localerc  plasma-org.kde.plasma.desktop-appletsrc  plasmashellrc  powerdevilrc  powermanagementprofilesrc  /etc/skel/.config
 
 
