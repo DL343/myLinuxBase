9545709e5ce1b89802fe88cb434503c2aa986dd9
DL343 <129131252+DL343@users.noreply.github.com>
2024-10-27 04:47:26 -0600

    update

--- Otros/iso/sesion/kdeConfig/getKDEConfig.sh
+++ Otros/iso/sesion/kdeConfig/getKDEConfig.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cp ./.gtkrc-2.0   /home/live/
+cp ./.gtkrc-2.0   /etc/skel/
+
+cd ./.config.fake/
 cp -r gtk-3.0/ gtk-4.0/ kdedefaults/ gtkrc gtkrc-2.0 kactivitymanagerd-pluginsrc kactivitymanagerdrc kded5rc kdeglobals krunnerrc ksmserverrc kwalletrc kwinrc plasma-org.kde.plasma.desktop-appletsrc  plasmashellrc /home/live/.config
 cp -r gtk-3.0/ gtk-4.0/ kdedefaults/ gtkrc gtkrc-2.0 kactivitymanagerd-pluginsrc kactivitymanagerdrc kded5rc kdeglobals krunnerrc ksmserverrc kwalletrc kwinrc plasma-org.kde.plasma.desktop-appletsrc  plasmashellrc /etc/skel/.config
 
