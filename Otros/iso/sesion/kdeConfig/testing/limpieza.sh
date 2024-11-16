5c7daa19a38aec93d2f38ebc03f84d4910e9a624
DL343 <129131252+DL343@users.noreply.github.com>
2024-11-16 16:34:28 -0600

    update

---
+++ Otros/iso/sesion/kdeConfig/testing/limpieza.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+rm   ./live/.face.icon
+
+
+rm   ./live/.config/autostart/Welcome.desktop
+
+
+
+rm -r   ./live/.config/bleachbit/
+
+rm -r   ./live/.config/kdeconnect/
+rm -r   ./live/.config/khtml/
