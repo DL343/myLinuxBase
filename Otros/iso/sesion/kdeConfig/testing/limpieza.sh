0db19da6d61e5c4af13cc3d72f649ef12f9288e6
DL343 <129131252+DL343@users.noreply.github.com>
2024-11-30 20:51:38 -0600

    update

---
+++ Otros/iso/sesion/kdeConfig/testing/limpieza.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+rm   ./live/.face.icon
+rm   ./live/.config/autostart/Welcome.desktop
+
+rm  -r ./live/Desktop/
+rm  -r ./live/Documents/
+rm  -r ./live/Downloads/
+rm  -r ./live/Music/
+rm  -r ./live/Pictures/
+rm  -r ./live/Public/
+rm  -r ./live/Templates/
+rm  -r ./live/Videos/
+
+
+rm -r   ./live/.config/bleachbit/
+rm -r   ./live/.config/kdeconnect/
+rm -r   ./live/.config/khtml/
+
+
+rm   ./live/.config/katerc
+rm   ./live/.config/spectaclerc
+rm   ./live/.config/user-dirs.dirs
+rm   ./live/.config/user-dirs.locale
+rm   ./live/.config/okularpartrc
+rm   ./live/.config/gwenviewrc
+rm   ./live/.config/discoverrc
+
+
+rm   ./live/.xsession-errors
+rm   ./live/.Xauthority
